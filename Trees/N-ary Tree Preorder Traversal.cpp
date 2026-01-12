75f047bf02399846fb3dab631274e499b463aeac
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-01-12 12:30:13 +0530

    Implement N-ary Tree Preorder Traversal

---
+++ Trees/N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    vector<int> preorder(Node* root) {
+        if (!root) return ans;
+
+        ans.push_back(root->val);
+        for (Node* child : root->children) {
+            preorder(child);
+        }
+        return ans;
+    }
+};
