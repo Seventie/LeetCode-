b68ee3e8c30fea2356493536142650af5a2d55b3
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-01-13 18:27:53 +0530

    Create 191. Number of 1 Bits.cpp

---
+++ Bit Manipulation/191. Number of 1 Bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans = 0 ;
+        while(n){
+            if(n%2 == 1) ans++ ;
+            n = n/2;
+        }
+        return ans;
+    }
+};
