8ec0d4413271820273a51e8ed053240483a90490
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-01-15 18:41:11 +0530

    Create 912. Sort an Array.cpp

---
+++ Arrays/912. Sort an Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
+
+
+I have already pratciced many sorts dont wanna wrap my head around them again 
