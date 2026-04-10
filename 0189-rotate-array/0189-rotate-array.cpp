347732318db86d372de710d4e10dfc2948e1a7e9
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-04-10 16:33:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k==0) return nums ;
+        int size =n nums.size();
+        k = k % size ;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
