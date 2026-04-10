052375fed48ad379768a67736922a804e1a9d975
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-04-10 16:52:19 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         vector<int> ans(n,1);
         vector<int> a(n,1);
-        vector<int> a(n,1);
+        vector<int> b(n,1);
         int p = 1 ;
         for(int x =0 ;x <n ; x++){
-            p = a[x]* p;
+            p = nums[x]* p;
             a[x] = p;
         }
-        int p = 1 ;
+        p = 1 ;
         for(int x =n-1 ;x >=0 ; x--){
-            p = a[x]* p;
-            a[x] = p;
+            p = nums[x]* p;
+            b[x] = p;
         }
         for(int i =0 ; i < n ; i++){
             if(i==0){
@@ -26,8 +26,7 @@ public:
                 ans[i] = a[i-1] * b[i+1];
             }
         }
+        
         return ans ;
     }
 };
-1,2,6,24
-24,24,12,4
