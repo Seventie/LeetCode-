530cb9a0be951265332d96de4a6284e3bee06ea1
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-04-14 12:48:38 +0530

    Time: 3 ms (52.72%), Space: 19.6 MB (27.76%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -4,11 +4,24 @@ class Solution:
         mp = defaultdict(int)
         for x in s :
             mp[x] += 1
-        maxheap = []
+        heap = []
         for x ,y in mp.items():
-            heapq.heappush(maxheap,(-y,x))
-        print(maxheap)
+            heapq.heappush(heap,[-y,x])
+        print(heap)
         prev = [-len(s)+1,'#'] 
         ans = ""
         while heap :
-            
+            if not heap :
+                break
+            current = heap[0]
+            ans += current[1]
+            current[0] += 1 
+            heapq.heappop(heap)
+            if prev[1] != '#' and prev[0] < 0:
+                heapq.heappush(heap,prev)
+            print(ans)
+            prev = current 
+        if len(ans) != len(s) :
+            return ""
+        return ans     
+
