e314a4148dcf8b5de4c2e0a475c77a8d734ca6b4
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-05-04 17:15:57 +0530

    Time: 0 ms (100%), Space: 19.3 MB (42.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,3 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m+n-2,n-1)
+        dp = [[1]*m for _ in range(n)]
+        if m == 1 or n == 1 :
+            return 1 
+        for x in range(n-2,-1,-1) :
+            for y in range(m-2,-1,-1) :
+                dp[x][y] = dp[x+1][y] + dp[x][y+1]
+        
+        return dp[0][0]
