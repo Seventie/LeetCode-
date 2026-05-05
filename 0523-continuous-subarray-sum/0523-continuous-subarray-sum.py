a3c4bde9e8b371bb94af7d21364d58bd1d231441
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-05-05 11:41:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,18 +1,16 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        if len(nums) < 2:
-            return False 
-        j = 0
-        psum = 0
-        pmap = {0: -1}   
-        while j < len(nums):
-            psum += nums[j]
-            rem = psum % k
-            if rem in pmap:
-                if j - pmap[rem] >= 2:   
-                    return True
-            else:
-                pmap[rem] = j 
-            j += 1
+        csum = 0 
+        mp = {0 : -1}
+
+        for x in range(len(nums)) :
+            csum += nums[x]
+            rem = csum % k 
+
+            if rem in mp :
+                if x - mp[rem] >= 2 :
+                    return True 
+            else :
+                mp[rem] = x
             
         return False
