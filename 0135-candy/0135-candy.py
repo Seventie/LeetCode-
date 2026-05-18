165603d8085fcb901c2861d6f6ea98a54bb507c0
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-05-18 12:31:33 +0530

    Time: 12 ms (82.5%), Space: 21.3 MB (77.49%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,34 +1,14 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        _sum = [0] * len(ratings)
         n = len(ratings)
-        if n == 1 : return 1 
-        _heap = []
-        for x in range(n) :
-            heapq.heappush(_heap,(ratings[x], x))
-        ans = 0 
-        while _heap :
-            rating , idx = heapq.heappop(_heap)
-            req = 1
-            if idx == 0 :
-                if ratings[idx] > ratings[idx + 1]:
-                    req += _sum[idx + 1]
+        candies = [1] * n 
 
-            elif idx == n - 1 :
-                if ratings[idx] > ratings[idx - 1]:
-                    req += _sum[idx - 1]
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = candies[i-1] + 1
 
-            else :
-                print("Hi")
-                _max = 0
-                if ratings[idx] > ratings[idx + 1] and ratings[idx] > ratings[idx - 1] :
-                    _max = max(_sum[idx + 1], _sum[idx - 1])     
-                elif ratings[idx] > ratings[idx + 1] :
-                    _max = _sum[idx + 1]
-                elif ratings[idx] > ratings[idx - 1] :
-                    _max = _sum[idx - 1]
-                req += _max 
-            _sum[idx] = req
-            ans += req 
-           
-        return ans 
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i], candies[i+1] + 1)
+        
+        return sum(candies)
