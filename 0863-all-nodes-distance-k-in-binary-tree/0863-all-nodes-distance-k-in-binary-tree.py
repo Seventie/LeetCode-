27149e8726418238112765cc1c9d4207752eec09
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-05-20 13:00:19 +0530

    Time: 49 ms (55.55%), Space: 20.1 MB (21.07%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -1,22 +1,32 @@
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         parent = {}
-        def dfs(node, par):
+        def search(node):
             if not node:
-                return
-            parent[node] = par
-            dfs(node.left, node)
-            dfs(node.right, node)
-        dfs(root, None)
-        temp = deque([(target, 0)])
-        vis = set([target])
+                return None
+            if node.left:
+                parent[node.left.val] = node
+            if node.right:
+                parent[node.right.val] = node
+            if node.val == target.val:
+                return node
+            left = search(node.left)
+            if left:
+                return left
+            return search(node.right)
         ans = []
-        while temp:
-            curr, lvl = temp.popleft()
-            if lvl == k:
-                ans.append(curr.val)
-            for nxt in [curr.left, curr.right, parent[curr]]:
-                if nxt and nxt not in vis:
-                    vis.add(nxt)
-                    temp.append((nxt, lvl + 1))
+        visited = set()
+        def dfs(node, cnt):
+            if not node or node.val in visited:
+                return
+            visited.add(node.val)
+            if cnt == k:
+                ans.append(node.val)
+                return
+            if node.val in parent:
+                dfs(parent[node.val], cnt + 1)
+            dfs(node.left, cnt + 1)
+            dfs(node.right, cnt + 1)
+        start = search(root)
+        dfs(start, 0)
         return ans
