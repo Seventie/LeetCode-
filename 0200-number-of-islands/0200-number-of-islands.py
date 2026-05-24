a78807dadb26c26e1477263356983c49c09f24c0
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-05-24 11:24:35 +0530

    Time: 241 ms (66.99%), Space: 21.6 MB (48.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,17 +2,22 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        moves = [[-1,0],[0,-1],[1,0],[0,1]]
-        def dfs(x,y):
-            if x < 0 or y < 0 or x >= n or y >= m or grid[x][y] != "1":
-                return
-            grid[x][y] = "@"
-            for i in moves:
-                dfs(x + i[0], y + i[1])
-        ans = 0
+        moves =[
+            (0,1),
+            (0,-1),
+            (1,0),
+            (-1,0)
+        ]
+        def dfs(x, y) :
+            if x < 0 or y < 0 or x >= n or y >= m or grid[x][y] != '1' :
+                return 
+            grid[x][y] = '#'
+            for dx, dy in moves :
+                dfs(x+ dx, y+ dy)
+        ans = 0 
         for x in range(n):
             for y in range(m):
-                if grid[x][y] == "1":
+                if grid[x][y] == '1':
                     dfs(x,y)
                     ans += 1
-        return ans
+        return ans 
