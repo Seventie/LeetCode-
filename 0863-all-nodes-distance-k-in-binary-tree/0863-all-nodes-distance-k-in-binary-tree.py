90bfcd82c643bd6d3d2daa9426a6d8acef85de9e
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-06-29 19:00:58 +0530

    Time: 44 ms (84.37%), Space: 20.1 MB (19.9%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -1,32 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        parent = {}
-        def search(node):
+        storage = {}
+        def finder(node, val):
             if not node:
                 return None
-            if node.left:
-                parent[node.left.val] = node
-            if node.right:
-                parent[node.right.val] = node
-            if node.val == target.val:
+
+            if node.val == val:
                 return node
-            left = search(node.left)
-            if left:
-                return left
-            return search(node.right)
+
+            if node.left:
+                storage[node.left.val] = node
+                left = finder(node.left, val)
+                if left:
+                    return left
+
+            if node.right:
+                storage[node.right.val] = node
+                right = finder(node.right, val)
+                if right:
+                    return right
+
+            return None
         ans = []
         visited = set()
-        def dfs(node, cnt):
+
+        def bfs(node, cnt):
             if not node or node.val in visited:
                 return
+
             visited.add(node.val)
+
             if cnt == k:
                 ans.append(node.val)
                 return
-            if node.val in parent:
-                dfs(parent[node.val], cnt + 1)
-            dfs(node.left, cnt + 1)
-            dfs(node.right, cnt + 1)
-        start = search(root)
-        dfs(start, 0)
-        return ans
+
+            if node.left:
+                bfs(node.left, cnt + 1)
+
+            if node.right:
+                bfs(node.right, cnt + 1)
+
+            if node.val in storage:
+                bfs(storage[node.val], cnt + 1)
+
+        curr = finder(root, target.val)
+        bfs(curr, 0)
+        return ans 
