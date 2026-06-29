c3f5831ba96e2ac78be5f88ce43ce1ea721fc930
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-06-29 19:25:46 +0530

    Time: 26 ms (76.62%), Space: 22.3 MB (38.11%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,34 +1,23 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        n = len(heights)
-        m = len(heights[0])
+        n, m = len(heights), len(heights[0])
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
         pacific = set()
         atlantic = set()
-        moves = [
-            (0, 1),
-            (0, -1),
-            (1, 0),
-            (-1, 0)
-        ]
-        def dfs(x, y, visited):
-            visited.add((x, y))
-            for dx, dy in moves:
+        def dfs(x, y, seen):
+            seen.add((x, y))
+            for dx, dy in dirs:
                 nx, ny = x + dx, y + dy
-
-                if (
-                    0 <= nx < n and
-                    0 <= ny < m and
-                    (nx, ny) not in visited and
-                    heights[nx][ny] >= heights[x][y]
-                ):
-                    dfs(nx, ny, visited)
-        for y in range(m):
-            dfs(0, y, pacific)
-            dfs(n - 1, y, atlantic)
-        for x in range(n):
-            dfs(x, 0, pacific)
-            dfs(x, m - 1, atlantic)
-        ans = []
-        for cell in pacific & atlantic:
-            ans.append(list(cell))
-        return ans
+                if (0 <= nx < n and 0 <= ny < m and
+                    (nx, ny) not in seen and
+                    heights[nx][ny] >= heights[x][y]):
+                    dfs(nx, ny, seen)
+        for i in range(n):
+            dfs(i, 0, pacific)
+        for j in range(m):
+            dfs(0, j, pacific)
+        for i in range(n):
+            dfs(i, m - 1, atlantic)
+        for j in range(m):
+            dfs(n - 1, j, atlantic)
+        return [[x, y] for (x, y) in pacific & atlantic]
