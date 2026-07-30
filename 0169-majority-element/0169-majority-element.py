6ba141e47d5f43e40cddd2a648b248e058285b20
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-07-30 18:21:39 +0530

    Time: 7 ms (57.21%), Space: 21.3 MB (18.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        length = len(nums)
-        map = defaultdict(int)
-        for x in nums :
-            map[x]+= 1 
-            if map[x] > length/2 :
-                return x 
-        
+        ele = None
+        cnt = 0
+        for x in nums:
+            if cnt == 0:
+                ele = x
+                cnt = 1
+            elif x == ele:
+                cnt += 1
+            else:
+                cnt -= 1
+
+        return ele
