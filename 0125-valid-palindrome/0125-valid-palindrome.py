7bee2c1e73ac480060ffd95664ae2be9e5b370f6
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-07-30 21:49:57 +0530

    Time: 9 ms (41.46%), Space: 19.6 MB (76.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,10 +6,5 @@ class Solution:
                 st += x.lower()
         i = 0 
         j = len(st) -1 
-        while i < j :
-            if st[i] == st[j]:
-                i+=1
-                j-=1
-            else :
-                return False 
-        return True 
+        
+        return st == st[::-1]
