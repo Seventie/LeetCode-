64e1294a825254137374068ba44302dbfd2b71b7
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-07-31 11:47:44 +0530

    Time: 3 ms (51.32%), Space: 19.4 MB (52.33%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,27 +1,22 @@
-import heapq
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        mp = defaultdict(int)
-        for x in s :
-            mp[x] += 1
-        heap = []
-        for x ,y in mp.items():
-            heapq.heappush(heap,[-y,x])
-        print(heap)
-        prev = [-len(s)+1,'#'] 
+        store = Counter(s)
+        h = []
+        for x, y in store.items() :
+            heapq.heappush(h, (-y, x))
         ans = ""
-        while heap :
-            if not heap :
-                break
-            current = heap[0]
-            ans += current[1]
-            current[0] += 1 
-            heapq.heappop(heap)
-            if prev[1] != '#' and prev[0] < 0:
-                heapq.heappush(heap,prev)
-            print(ans)
-            prev = current 
-        if len(ans) != len(s) :
-            return ""
-        return ans     
+        prev = (None, None)
+
+        while h :
+            a, b = heapq.heappop(h)
+            ans += b
+            a += 1 
+            if prev[0] is not None :
+                heapq.heappush(h,prev)
+                prev = (None, None)
+            if a < 0 :
+                prev = (a, b)
+        if len(ans) != len(s) : return ""
+        return ans 
+                
 
