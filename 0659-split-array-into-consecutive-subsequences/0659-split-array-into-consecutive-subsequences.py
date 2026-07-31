f94f23b09f52c293a5f07a71eda83ed9b6550afb
Shaik Abdus Sattar <abdussattarshaik07@gmail.com>
2026-07-31 12:05:21 +0530

    Time: 21 ms (83.63%), Space: 20.3 MB (75%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -1,19 +1,28 @@
+from collections import Counter
+
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        freq = Counter(nums)
-        tails = defaultdict(int)
-        for num in nums:
-            if freq[num] == 0:
+        freq = Counter(nums)      # numbers left
+        need = Counter()          # subsequences waiting for this number
+
+        for x in nums:
+
+            if freq[x] == 0:
                 continue
-            if tails[num] > 0:
-                tails[num] -= 1
-                tails[num + 1] += 1
-            elif freq[num + 1] > 0 and freq[num + 2] > 0:
-                freq[num + 1] -= 1
-                freq[num + 2] -= 1
-                tails[num + 3] += 1
+
+            # use x
+            freq[x] -= 1
+
+            # extend an existing subsequence
+            if need[x] > 0:
+                need[x] -= 1
+                need[x + 1] += 1
+
+            elif freq[x + 1] > 0 and freq[x + 2] > 0:
+                freq[x + 1] -= 1
+                freq[x + 2] -= 1
+                need[x + 3] += 1
+
             else:
                 return False
-            freq[num] -= 1
         return True
-        
